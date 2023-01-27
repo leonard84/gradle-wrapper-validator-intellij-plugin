4d273a657d22d0947f7b845c78b632153d9cd7ea
Leonard Brünings <leonard.bruenings@gradle.com>
2023-01-27 15:19:32 +0100

    fixup! Change package and rename files

--- src/test/kotlin/org/gradle/intellij/plugin/gradlewrappervalidator/WrapperValidatorPluginTest.kt
+++ src/test/kotlin/org/gradle/intellij/plugin/gradlewrappervalidator/WrapperValidatorPluginTest.kt
@@ -1,4 +1,4 @@
-package com.github.leonard84.gradlewrappervalidatorintellijplugin
+package org.gradle.intellij.plugin.gradlewrappervalidator
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.openapi.components.service
